c31eae8f73e812fa627d16ebab8a487ff268db3e
George <symyon89@yahoo.com>
2022-03-22 20:28:10 +0200

    asd

--- src/main/java/beans/persoana/exceptions/BusinessControllerExceptionHandler.java
+++ src/main/java/beans/persoana/exceptions/BusinessControllerExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
+
 public class BusinessControllerExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(MyBusinessException.class)
